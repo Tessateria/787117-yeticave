4ec9ebb93b75c70e5198bab8c0e44f1804aed7f2
Tessa <tasyanikitchuk@gmail.com>
2018-11-15 14:31:25 +0200

    + кавычки

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $user_avatar = 'img/user.jpg';
         <nav class="user-menu">
             <?php if ($is_auth): ?>
             <div class="user-menu__image">
-                <img src=<?=$user_avatar?> width="40" height="40" alt="Пользователь">
+                <img src="<?=$user_avatar?>" width="40" height="40" alt="Пользователь">
             </div>
             <div class="user-menu__logged">
                 <p><?=$user_name?></p>
