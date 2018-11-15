0269ea841eb34f059f6be0eab72083e7fe3757c8
Tessa <tasyanikitchuk@gmail.com>
2018-11-15 13:25:38 +0200

    + вывод аватарки из переменной

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = 'Наталья'; 
+$user_name = 'Наталья';
 $user_avatar = 'img/user.jpg';
 ?>
 <!DOCTYPE html>
@@ -30,7 +30,7 @@ $user_avatar = 'img/user.jpg';
         <nav class="user-menu">
             <?php if ($is_auth): ?>
             <div class="user-menu__image">
-                <img src="img/user.jpg" width="40" height="40" alt="Пользователь">
+                <img src=<?=$user_avatar?> width="40" height="40" alt="Пользователь">
             </div>
             <div class="user-menu__logged">
                 <p><?=$user_name?></p>
